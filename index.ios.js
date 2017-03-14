35b619ec516a1040f4dbd5e62bb65adef43e0608
JesterDunice <nikita.dudin@gmail.com>
2017-03-14 12:56:15 +0300

    initial

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+/**
+ * Calendar React Native App
+ */
+
+import React from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import Main from './src/main';
+
+
+
+AppRegistry.registerComponent('Calendar', () => Main);
